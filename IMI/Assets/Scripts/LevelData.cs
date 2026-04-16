af0f09da21dd524fb037fe1536d80dfab06721e5
Caroline <carolinebastiaa@gmail.com>
2026-04-16 05:41:34 -0700

    Added water node and monster

--- IMI/Assets/Scripts/LevelData.cs
+++ IMI/Assets/Scripts/LevelData.cs
@@ -7,6 +7,7 @@ public class LevelData
     public string levelName;
     public ElementType enemyType;
     public Sprite enemySprite;
+    public Sprite backgroundSprite;
     public int enemyHP;
     public int enemyAttack;
 }
