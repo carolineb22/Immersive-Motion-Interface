6ef98c5c3aa6483f67a4fc18344a0f04af05c117
Caroline <carolinebastiaa@gmail.com>
2026-04-15 18:25:16 -0700

    added font and styled main menu

---
+++ IMI/Assets/Scripts/MainMenu.cs
@@ -0,0 +1,17 @@
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MainMenu : MonoBehaviour
+{
+    public void StartGame()
+    {
+        SceneManager.LoadScene(1); // loads  battle scene
+    }
+
+    public void QuitGame()
+    {
+        Debug.Log("Quit Game"); // works in editor
+
+        Application.Quit(); // works in build
+    }
+}
